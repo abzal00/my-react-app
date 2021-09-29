652930e6b26ee22fde76401dc54b5984025543bf
abzal00 <mk.gam@mail.ru>
2021-09-29 13:34:47 +0600

    Updates

---
+++ precache-manifest.c809fc3df91055046ae5dc0111467d73.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e69a2d834d526bb63a5c",
+    "url": "/abzal00/my-react-app.git/static/css/main.a1a1d238.chunk.css"
+  },
+  {
+    "revision": "e69a2d834d526bb63a5c",
+    "url": "/abzal00/my-react-app.git/static/js/main.e69a2d83.chunk.js"
+  },
+  {
+    "revision": "803cb17e84610d21a03b",
+    "url": "/abzal00/my-react-app.git/static/css/1.5936d6a4.chunk.css"
+  },
+  {
+    "revision": "803cb17e84610d21a03b",
+    "url": "/abzal00/my-react-app.git/static/js/1.803cb17e.chunk.js"
+  },
+  {
+    "revision": "4a54d5ee58f25caa8640",
+    "url": "/abzal00/my-react-app.git/static/css/2.403b0e14.chunk.css"
+  },
+  {
+    "revision": "4a54d5ee58f25caa8640",
+    "url": "/abzal00/my-react-app.git/static/js/2.4a54d5ee.chunk.js"
+  },
+  {
+    "revision": "1ec197ab74a4624c6ea4",
+    "url": "/abzal00/my-react-app.git/static/js/3.1ec197ab.chunk.js"
+  },
+  {
+    "revision": "bdd6e81fe8d9871441fa",
+    "url": "/abzal00/my-react-app.git/static/js/runtime~main.bdd6e81f.js"
+  },
+  {
+    "revision": "5b753808af3d195111a9fce238e106b6",
+    "url": "/abzal00/my-react-app.git/static/media/Ava.5b753808.jfif"
+  },
+  {
+    "revision": "504b535c80c8095184901c10e561233b",
+    "url": "/abzal00/my-react-app.git/static/media/prelader.504b535c.gif"
+  },
+  {
+    "revision": "83f1567fe98abffe4373f05a9f0238f4",
+    "url": "/abzal00/my-react-app.git/index.html"
+  }
+];
